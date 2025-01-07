f092456a8ce0323b55e699cbeae09e24394fef8b
Stéphane B <20hertz@users.noreply.github.com>
2025-01-07 10:40:21 -0500

    Initial Commit

---
+++ Gym Time/Gym_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_TimeApp.swift
+//  Gym Time
+//
+//  Created by Stéphane on 2025-01-07.
+//
+
+import SwiftUI
+
+@main
+struct Gym_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
